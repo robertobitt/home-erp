d180e787de3f1115ef694a5b1365982d5203cb48
roberto <robertobbtt@gmail.com>
2026-09-19 18:51:23 -0300

    Use Pantry to manager data lots

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,33 @@ public class Main {
         System.out.println("===HomeERP - Teste  de controle de estoque (PCP) ===\n");
         // 1. Criar os produtos base (BaseProduct)
         // Arroz: Giro interno de 30 dias | Alerta com 5 dias de antecedência
-        BaseProduct arroz = new BaseProduct("Arroz agulhinha","grãos","kg", 30, 5);
+        BaseProduct arroz = new BaseProduct("Arroz Agulhinha","Grãos","kg", 30, 5);
 
         // Leite: Giro interno de 10 dias | Alerta com 3 dias de antecedência
-        BaseProduct leite = new BaseProduct("Leite Integral","laticíneos","L", 30, 5, 5.0,20.0);
+        BaseProduct leite = new BaseProduct("Leite Integral","Laticíneos","L", 30, 5, 5.0,20.0);
+
+        // Pimenta: Usando o construtor secundário (sem min/max informados -> defaults to null)
+        BaseProduct pimenta = new BaseProduct("Pimenta do Reino","Temperos", "g", 180, 15 );
 
         //Local dates of entered
         LocalDate dataDeEntrada = LocalDate.now();
 
-        InventoryItem loteArroz = new InventoryItem(arroz, "Tio João", 5, 25.90, dataDeEntrada);
+
+        InventoryItem loteArroz = new InventoryItem(arroz, "Camil", 5, 25.90, dataDeEntrada);
 
         //Overload constructor
-        LocalDate validadeRotuloLeite = dataDeEntrada.plusDays(7);
+        LocalDate validadeRotuloLeite = dataDeEntrada.minusDays(8);
         InventoryItem loteLeite = new InventoryItem(leite, "Parmalat", 12, 4.50, dataDeEntrada, validadeRotuloLeite);
+
+        InventoryItem lotePimenta = new InventoryItem(pimenta, "Kitano", 100.0, 8.50, dataDeEntrada);
+
+        Pantry despensa = new Pantry();
+        System.out.println("Cadastrando lotes na despensa");
+        despensa.addLot(loteArroz);
+        despensa.addLot(loteLeite);
+        despensa.addLot(lotePimenta);
+
+        despensa.printStockByLot();
 
         //show the infos about batch dates
         System.out.println("---Lote de arroz criado---");
---
+++ src/Pantry.java
@@ -0,0 +1,45 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pantry {
+
+    private List<InventoryItem> items;
+
+    public Pantry() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addLot(InventoryItem item) {
+        this.items.add(item);
+        System.out.println("Lote de " + item.getBaseProduct().getName() + " (" + item.getBrand() + "adicionado a despensa.");
+
+    }
+
+    public void printStockByLot() {
+        System.out.println("=== RELATÓRIO: ESTOQUE DETALHADO POR LOTE ===");
+        if (items.isEmpty()) {
+            System.out.println("A despensa está vazia.");
+            return;
+        }
+
+        for (int i = 0; i < items.size(); i++) {
+            InventoryItem item = items.get(i);
+            System.out.println("Lote #" + (i + 1) + " | " + item.getBaseProduct().getName() + " (" + item.getBrand() + ") - Qtd: " + item.getQuantity() + item.getBaseProduct().getUnitOfMeasure() + " | Data Entrada: " + item.getEntryDate() + " | Validade Interna: " + item.getInternalExpirationDate());
+        }
+    }
+
+    public List<InventoryItem> getWarningItems(LocalDate currentDate) {
+        List<InventoryItem> warningList = new ArrayList<>();
+
+        for (InventoryItem item : items) {
+            if (item.isEarlyWarningTrigged(currentDate)) {
+                warningList.add(item);
+            }
+        }
+        return warningList;
+    }
+    public List<InventoryItem> getItems(){
+        return items;
+    }
+}
